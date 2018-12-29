6a4c9336fc2cc70a7126439dbce58cda45fc8e1d
shencang <2630610009@qq.com>
2018-12-29 17:07:07 +0800

    finish

--- src/BoardTools/ClassUtil.java
+++ src/BoardTools/ClassUtil.java
@@ -1,4 +1,168 @@
 package BoardTools;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
 public class ClassUtil {
+    public static List<Class<?>> getClassList(String pkgName,
+                                              boolean isRecursive,
+                                              Class<? extends Annotation> annotation) {
+        List<Class<?>> classList = new ArrayList<Class<?>>();
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        try {
+            // 按文件的形式去查找
+            String strFile = pkgName.replaceAll("\\.", "/");
+            Enumeration<URL> urls = loader.getResources(strFile);
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (url != null) {
+                    String protocol = url.getProtocol();
+                    String pkgPath = url.getPath();
+                    System.out.println("protocol:" + protocol + " path:" + pkgPath);
+                    if ("file".equals(protocol)) {
+                        // 本地自己可见的代码
+                        findClassName(classList, pkgName, pkgPath, isRecursive, annotation);
+                    } else if ("jar".equals(protocol)) {
+                        // 引用第三方jar的代码
+                        findClassName(classList, pkgName, url, isRecursive, annotation);
+                    }
+                }
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return classList;
+    }
+
+    private static void findClassName(List<Class<?>> classList,
+                                      String pkgName,
+                                      String pkgPath,
+                                      boolean isRecursive,
+                                      Class<? extends Annotation> annotation) {
+        if (classList == null) {
+            return;
+        }
+        File[] files = filterClassFiles(pkgPath);// 过滤出.class文件及文件夹
+        System.out.println("files:" + ((files == null) ? "null" : "length=" + files.length));
+        if (files != null) {
+            for (File f : files) {
+                String fileName = f.getName();
+                if (f.isFile()) {
+                    // .class 文件的情况
+                    String className = getClassName(pkgName, fileName);
+                    addClassName(classList, className, annotation);
+                } else {
+                    // 文件夹的情况
+                    if (isRecursive) {
+                        // 需要继续查找该文件夹/包名下的类
+                        String subPkgName = pkgName + "." + fileName;
+                        String subPkgPath = pkgPath + "/" + fileName;
+                        findClassName(classList, subPkgName, subPkgPath, true, annotation);
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 第三方Jar类库的引用。
+     *
+     * @throws IOException
+     */
+    public static void findClassName(List<Class<?>> classList,
+                                     String pkgName, URL url,
+                                     boolean isRecursive,
+                                     Class<? extends Annotation> annotation) throws IOException {
+        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+        JarFile jarFile = jarURLConnection.getJarFile();
+        System.out.println("jarFile:" + jarFile.getName());
+        Enumeration<JarEntry> jarEntries = jarFile.entries();
+        while (jarEntries.hasMoreElements()) {
+            JarEntry jarEntry = jarEntries.nextElement();
+            String jarEntryName = jarEntry.getName(); // 类似：sun/security/internal/interfaces/TlsMasterSecret.class
+            String className = jarEntryName.replace("/", ".");
+            int endIndex = className.lastIndexOf(".");
+            String prefix = null;
+            if (endIndex > 0) {
+                String prefix_name = className.substring(0, endIndex);
+                endIndex = prefix_name.lastIndexOf(".");
+                if (endIndex > 0) {
+                    prefix = prefix_name.substring(0, endIndex);
+                }
+            }
+            if (prefix != null && jarEntryName.endsWith(".class")) {
+//				System.out.println("prefix:" + prefix +" pkgName:" + pkgName);
+                if (prefix.equals(pkgName)) {
+                    System.out.println("jar entryName:" + jarEntryName);
+                    addClassName(classList, className, annotation);
+                } else if (isRecursive && prefix.startsWith(pkgName)) {
+                    // 遍历子包名：子类
+                    System.out.println("jar entryName:" + jarEntryName + " isRecursive:" + isRecursive);
+                    addClassName(classList, className, annotation);
+                }
+            }
+        }
+    }
+
+    private static File[] filterClassFiles(String pkgPath) {
+        if (pkgPath == null) {
+            return null;
+        }
+        // 接收 .class 文件 或 类文件夹
+        return new File(pkgPath).listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory();
+            }
+        });
+    }
+
+    private static String getClassName(String pkgName, String fileName) {
+        int endIndex = fileName.lastIndexOf(".");
+        String classNames = null;
+        if (endIndex >= 0) {
+            classNames = fileName.substring(0, endIndex);
+        }
+        String className = null;
+        if (classNames != null) {
+            className = pkgName + "." + classNames;
+        }
+        return className;
+    }
+
+    private static void addClassName(List<Class<?>> classList,
+                                     String className,
+                                     Class<? extends Annotation> annotation) {
+        if (classList != null && className != null) {
+            Class<?> clazz = null;
+            try {
+                clazz = Class.forName(className);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+//			System.out.println("isAnnotation=" + clazz.isAnnotation() +" author:" + clazz.isAnnotationPresent(author.class));
+
+            if (clazz != null) {
+                if (annotation == null) {
+                    classList.add(clazz);
+                    System.out.println("add:" + clazz);
+                } else if (clazz.isAnnotationPresent(annotation)) {
+                    classList.add(clazz);
+                    System.out.println("add annotation:" + clazz);
+                }
+            }
+        }
+    }
 }
--- src/MainT.java
+++ src/MainT.java
@@ -1,2 +1,185 @@
+import BoardTools.Board;
+import BoardTools.ClassUtil;
+import Shapes.Shape;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainT  {
+    private static GC gcMain = null;
+    private static int startX;
+    private static int startY;
+    private static boolean leftButtonDown = false;
+    private static int lastWidth;
+    private static int lastHeight;
+    private static String shapeType = "Rect";
+
+    public static void main(String[] args) {
+        List listClass = null;
+        String pkg = "Shapes";
+        listClass = ClassUtil.getClassList(pkg, true, null);
+
+        ArrayList<String> shapeTypes = new ArrayList<String>();
+        for (Object object : listClass) {
+            String name = ((Class<?>) object).getName();
+            if (!name.equals("Shapes.Shape")) {
+                shapeTypes.add(name);
+            }
+        }
+
+        Display display = Display.getDefault();
+        Shell shell = new Shell();
+
+        gcMain = new GC(shell);
+        Board board = new Board(shell, gcMain);
+
+        shell.addPaintListener(new PaintListener() {
+            public void paintControl(PaintEvent arg0) {
+                board.Refresh();
+            }
+        });
+
+        // Init vars
+        shell.addMouseMoveListener(new MouseMoveListener() {
+            public void mouseMove(MouseEvent arg0) {
+                if (leftButtonDown) {
+                    gcMain.setLineStyle(SWT.LINE_DOT);
+                    gcMain.setForeground(shell.getBackground());
+                    gcMain.drawRectangle(startX, startY, lastWidth, lastHeight);
+                    gcMain.setForeground(display.getSystemColor(SWT.COLOR_BLUE));
+                    gcMain.drawRectangle(startX, startY, arg0.x - startX, arg0.y - startY);
+                    lastWidth = arg0.x - startX;
+                    lastHeight = arg0.y - startY;
+                    gcMain.setLineStyle(SWT.LINE_SOLID);
+                }
+            }
+        });
+        shell.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseUp(MouseEvent e) {
+                if (e.button == 1) {
+                    leftButtonDown = false;
+                    int width = e.x - startX;
+                    int height = e.y - startY;
+
+                    ////////////////////////////////////////////////////////////////
+                    // erase range rect
+                    gcMain.setLineStyle(SWT.LINE_DOT);
+                    gcMain.setForeground(shell.getBackground());
+                    gcMain.drawRectangle(startX, startY, width, height);
+                    gcMain.setLineStyle(SWT.LINE_SOLID);
+                    ////////////////////////////////////////////////////////////////
+
+                    gcMain.setForeground(display.getSystemColor(SWT.COLOR_BLACK));
+                    Shape shape;
+                    try {
+                        Class<?> shapeClass = Class.forName(shapeType);
+                        Object oShape = shapeClass.newInstance();
+                        shape = (Shape) oShape;
+                        shape.setTop(startX);
+                        shape.setLeft(startY);
+                        shape.setWidth(e.x - startX);
+                        shape.setHeight(e.y - startY);
+                        shape.setGcMain(gcMain);
+                    } catch (Exception ex) {
+                        shape = null;
+                    }
+
+                    if (shape != null) {
+                        board.InsertShape(shape);
+                        board.Refresh();
+                    }
+                    shell.setCursor(new Cursor(null, 0));
+                }
+            }
+
+            @Override
+            public void mouseDown(MouseEvent e) {
+                if (e.button == 1) {
+                    shell.setCursor(new Cursor(null, SWT.CURSOR_CROSS));
+                    leftButtonDown = true;
+                    startX = e.x;
+                    startY = e.y;
+                }
+
+            }
+        });
+        shell.setSize(800, 600);
+        shell.setText("DrawBoard");
+        shell.setLayout(null);
+
+        Button btnOpen = new Button(shell, SWT.NONE);
+        btnOpen.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                try {
+                    board.Open("D:\\BOARD.shape");
+                } catch (Exception ex) {
+                    return;
+                }
+            }
+        });
+        btnOpen.setBounds(10, 525, 80, 27);
+        btnOpen.setText("Open");
+
+        Button btnSave = new Button(shell, SWT.NONE);
+        btnSave.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                try {
+                    board.Save("D:\\BOARD.shape");
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                    return;
+                }
+
+            }
+        });
+        btnSave.setBounds(107, 525, 80, 27);
+        btnSave.setText("Save");
+
+        ////////////////////////////////////////////////////////
+        // add button by shapeTypes
+        int indexButton = 0;
+        for (String strClass : shapeTypes) {
+            Button btn = new Button(shell, SWT.NONE);
+            btn.addSelectionListener(new SelectionAdapter() {
+                @Override
+                public void widgetSelected(SelectionEvent e) {
+                    shapeType = strClass;
+                }
+            });
+            btn.setBounds(84 * indexButton, 0, 80, 27);
+            indexButton++;
+            try {
+                Class<?> shapeClass = Class.forName(strClass);
+                Method method = shapeClass.getMethod("getToolText");
+                btn.setText(method.invoke(null, null).toString());
+                btn.setData("shapeType", strClass);
+            } catch (Exception e) {
+                btn.setText(strClass);
+                btn.setData("shapeType", strClass);
+            }
+
+
+        }
+
+        shell.open();
+        shell.layout();
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+    }
+
+
 }
